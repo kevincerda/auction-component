6990bfda4a9c500646426479f0e1f8517499f542
Joseph An <josephan1221@gmail.com>
2018-06-02 21:17:47 -0700

    Add route for handling product name as param
    In order to serve up data with both a product's id and name, need to add
    route that handles that endpoint

--- server/router/index.js
+++ server/router/index.js
@@ -5,6 +5,10 @@ router.route('/auction/product/:id')
   .get(AuctionController.GET)
   .post(AuctionController.POST)
 
+router.route('/auction/product/:name')
+  .get(AuctionController.GET)
+  .post(AuctionController.POST)
+
 router.route('/auction/bid')
   .get(BidController.GET)
   .post(BidController.POST)
