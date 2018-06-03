4a1a4626874e0355332cf4bdcc69496cfa25ae80
Joseph An <josephan1221@gmail.com>
2018-06-02 18:34:53 -0700

    Add rest of styles for component

--- client/src/components/Auction.jsx
+++ client/src/components/Auction.jsx
@@ -110,10 +110,10 @@ class Auction extends React.Component {
     return (
       <div styleName="auction-container">
         <div styleName="item-name">{this.state.name}</div><hr />
-        <div>
+        <div styleName="info-container">
           <div>
             <div styleName="col-1">Condition: </div>
-            <div styleName="col-2">{this.state.condition}</div>
+            <div styleName="col-2"><div styleName="condition">{this.state.condition}</div></div>
           </div>
           <div>
             <div styleName="col-1">Time left: </div>
@@ -123,24 +123,26 @@ class Auction extends React.Component {
         <div styleName="bid-container">
           <div>
             <div styleName="col-1">Current bid: </div>
-            <div styleName="col-2">${this.state.currentBid}</div>
+            <div styleName="col-2"><span styleName="current-bid">${this.state.currentBid}</span></div>
             <div styleName="col-3">[ <a href="#">{this.state.bids} bids </a> ]</div>
           </div>
-          <form onSubmit={this.handleBidSubmit}>
-            <div styleName="col-1"></div>
-            <div styleName="col-2"><input onChange={this.handleBidChange}/></div>
-            <div styleName="col-3"><button>Place Bid</button></div>
-          </form>
+          <div styleName="bid-form">
+            <form onSubmit={this.handleBidSubmit}>
+              <div styleName="col-1"></div>
+              <div styleName="col-2"><input styleName="bid-input" onChange={this.handleBidChange}/></div>
+              <div styleName="col-3"><button styleName="place-bid">Place bid</button></div>
+            </form>
+          </div>
           <div>
             <div styleName="col-1"></div>
-            <div styleName="col-2">Enter ${Number(this.state.currentBid) + 0.01} or more</div>
+            <div styleName="col-2"><span styleName="instruction">Enter ${Number(this.state.currentBid) + 0.01} or more</span></div>
             <div styleName="col-3"></div>
           </div>
           <div>
             <div styleName="col-1"></div>
             <div styleName="col-2"></div>
             <div styleName="col-3">
-              <button onClick={this.addWatcher}>Add to watch list</button>
+              <button styleName="add-watcher" onClick={this.addWatcher}><i class="fa fa-eye"></i>    Add to watch list</button>
             </div>
           </div>
           <div styleName="watchers">{this.state.watchers} watchers</div>
