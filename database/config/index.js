64392887c2afd8b69f1479bb8fab302fa8538bae
Kevin <kevin@cerda.io>
2018-06-23 15:18:13 -0700

    Changed database credentials to my local machine's.

--- database/config/index.js
+++ database/config/index.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('auction', 'postgres', '', {
+const db = new Sequelize('auction', 'em', 'mars', {
   host: 'db',
   dialect: 'postgres'
 });
 
 db.authenticate()
   .then(() => console.log('db authenticated'))
-  .catch(err => console.log('error authenticating db'))
+  .catch(err => console.log('error authenticating db', err))
   
 module.exports = {
   db: db
