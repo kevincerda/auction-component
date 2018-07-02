2a3ca326209589cc1e88baefd3ab0d4988e9829c
Kevin <kevin@cerda.io>
2018-07-01 22:03:18 -0700

    Routes defined.

--- server/router/index.js
+++ server/router/index.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
-const { AuctionController, BidController } = require('../controllers');
+const { productCtrl, productById } = require('../controllers/productCtrl.js');
+const { bidCtrl, bidById } = require('../controllers/bidCtrl.js');
 
-router.route('/auction/product')
-  .get(AuctionController.GET)
-  .post(AuctionController.POST)
+router.route('/auction/products')
+  .post(productCtrl.POST)
 
-router.route('/auction/bid')
-  .get(BidController.GET)
-  .post(BidController.POST)
+router.route('/auction/product/:id')
+  .get(productById.GET)
+  .put(productById.PUT)
+  .delete(productById.DELETE)
+
+router.route('/auction/bids')
+  .post(bidCtrl.POST)
+  .put(bidCtrl.PUT)
+  .delete(bidCtrl.DELETE)
+
+router.route('/auction/bid/:id')
+  .get(bidById.GET)
 
 module.exports = {
-  router: router
+  router
 };
