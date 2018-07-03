80890677b9e7a9b6eb14465e480e740e09e0726d
Kevin <kevin@cerda.io>
2018-07-02 18:35:36 -0700

    New Relic added.

--- database/mongodb/config/index.js
+++ database/mongodb/config/index.js
@@ -1,14 +1,27 @@
+require('newrelic');
 const MongoClient = require('mongodb').MongoClient;
 const URI = 'mongodb://localhost:27017/auction';
-let _db, _products, _bids;
+let _db, _products, _bids, productsId;
+// require('../../seeds/genProductData.js');
 
 MongoClient.connect(URI, { useNewUrlParser: true })
   .then(client => {
     console.log('Successfully connected to mongoDB');
 
     _db = client.db();
-    _products = _db.collection('products');
+    _products = _db.collection('products')
     _bids = _db.collection('bids');
+
+    // _products.createIndex({ id: -1 }, { name: 'products', unique: true })
+    // .then(name => console.log(`Successfully created index for ${name}`))
+    // .catch(err => console.error(`Error creating index for ${name}`, err));
+
+    // _products.findOne({}, { fields: ['_id'], sort: [['_id', 'desc']] })
+    // .then(({ _id }) => {
+    //   productsId = _id + 1;
+    //   console.log('The current id is:', _id);
+    // });
+
   })
   .catch(err => {
     console.log('Error connecting to mongoDB');
@@ -22,5 +35,6 @@ const products = () => _products;
 module.exports = {
   db,
   bids,
-  products
+  products,
+  productsId
 }
